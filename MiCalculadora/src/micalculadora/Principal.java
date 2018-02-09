795b836b4634024e4e35a6e6e9caee6dea841381
Estudiantes <Estudiantes@Labing50405.udistrital.edu.co>
2018-02-09 11:45:31 -0500

    Creacion de objeto

--- MiCalculadora/src/micalculadora/Principal.java
+++ MiCalculadora/src/micalculadora/Principal.java
@@ -1,8 +1,13 @@
 package micalculadora;
 
 public class Principal {
-    public static void main(String[] args){
-        
-    } 
+
+    public static void main(String[] args) {
+        //Crear objeto clase *calculadora*
+
+        Calculadora miCalculadora;  //Definicion objeto
+        miCalculadora = new Calculadora(); //Contruccion objeto
+
+    }
 
 }
