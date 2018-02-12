ae294be8589aec4b41b8f0ac068f5e63ddd27601
Estudiantes <Estudiantes@Labing70210>
2018-02-12 17:37:01 -0500

    Calculadora trigo2

---
+++ CalculadorII/src/Calculadora/Principal.java
@@ -0,0 +1,32 @@
+package Calculadora;
+
+import java.util.*;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        //Crear objeto clase *calculadora*
+
+        Calculadora miCalculadora;  //Definicion objeto
+        miCalculadora = new Calculadora(); //Contruccion objeto
+
+        //Forma de crear un objeto de la clase escanner
+        Scanner miEscaner;
+        miEscaner = new Scanner(System.in);
+
+        int entrada;
+
+        entrada = miEscaner.nextInt();
+        miCalculadora.setX(entrada);
+
+        entrada = miEscaner.nextInt();
+        miCalculadora.setY(entrada);
+        
+        miCalculadora.sumar();
+        
+        System.out.println(miCalculadora.getR());
+
+        CalculadoraTrigonometrica miTrigo=new CalculadoraTrigonometrica();
+    }
+
+}
